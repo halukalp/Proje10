61ee9e5fe5e54408a6e21c9032df6dff7c9330cc
halukalp <haluk.allp@gmail.com>
2022-10-31 10:52:54 +0300

    initial commit

--- src/com/migros/ucg/oracle/migros/db_yonetim/program/Main.java
+++ src/com/migros/ucg/oracle/migros/db_yonetim/program/Main.java
@@ -1,8 +1,11 @@
 package com.migros.ucg.oracle.migros.db_yonetim.program;
 
+import com.migros.ucg.utility.Util;
+
 public class Main
 {
-    public static void main(String[] args) {
-
+    public static void main(String[] args)
+    {
+        System.out.println( Util.isValidTCKN("23503220782") );
     }
 }
--- src/com/migros/ucg/utility/Util.java
+++ src/com/migros/ucg/utility/Util.java
@@ -5,7 +5,59 @@ import java.util.List;
 
 public final class Util
 {
+    private static final int TCKN_HANE = 11;
+
     private Util() {}
+
+    // kaynak: Ahmet Bütün
+    private static boolean isValidTCKN(Long TCKN)
+    {
+        String tmp = TCKN.toString();
+
+        if (tmp.length() == TCKN_HANE)
+        {
+            int totalOdd = 0;
+
+            int totalEven = 0;
+
+            for (int i = 0; i < 9; i++) {
+                int val = Integer.valueOf(tmp.substring(i, i + 1));
+
+                if (i % 2 == 0) {
+                    totalOdd += val;
+                } else {
+                    totalEven += val;
+                }
+            }
+
+            int total = totalOdd + totalEven
+                    + Integer.valueOf(tmp.substring(9, 10));
+
+            int lastDigit = total % 10;
+
+            if (tmp.substring(10).equals(String.valueOf(lastDigit)))
+            {
+                int check = (totalOdd * 7 - totalEven) % 10;
+
+                if (tmp.substring(9, 10).equals(String.valueOf(check)))
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean isValidTCKN(String TCKN)
+    {
+        if (null!=TCKN && TCKN.matches("^([1-9]{1}[0-9]{10})$"))
+        {
+            return isValidTCKN(Long.valueOf(TCKN));
+        }
+
+        return false;
+    }
 
     public static void showGeneralExceptionInfo(Exception e)
     {
